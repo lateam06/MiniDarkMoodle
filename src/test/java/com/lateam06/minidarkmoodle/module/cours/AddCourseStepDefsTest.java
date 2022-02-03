77f20b99099463ae529557771ee2a5f009c0dbb4
Antoine Vidal-Mazuy <antoine.vidalma@gmail.com>
2022-02-03 18:28:20 +0100

    first stepdef

--- src/test/java/com/lateam06/minidarkmoodle/module/cours/AddCourseStepDefsTest.java
+++ src/test/java/com/lateam06/minidarkmoodle/module/cours/AddCourseStepDefsTest.java
@@ -1,15 +1,18 @@
-package com.lateam06.minidarkmoodle;
+package com.lateam06.minidarkmoodle.module.cours;
 
 import io.cucumber.java.en.Given;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.lateam06.minidarkmoodle.module.Module;
+import com.lateam06.minidarkmoodle.user.Teacher;
+
 class AddCourseStepDefsTest {
 
-    /*private Module m;
+    private Module m;
     private Teacher marcel;
-    private Course c;*/
+    private Course c;
 
     public AddCourseStepDefsTest() {
 
@@ -17,7 +20,8 @@ class AddCourseStepDefsTest {
 
     @Given("a teacher named {string} and with teacher ID {int}")
     public void givenATeacher(String teacherName, int teacherId) {
-
+        this.m = new Module("test", "test module for gherkin", 0);
+        this.marcel = new Teacher(teacherName, "123", teacherId);
     }
 
 }
