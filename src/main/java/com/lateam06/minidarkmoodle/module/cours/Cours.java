e8ffcfc708808e6aaca79bfb578bc5493ad161fa
Antoine Vidal-Mazuy <antoine.vidalma@gmail.com>
2022-02-03 17:29:50 +0100

    Added abstracy

--- src/main/java/com/lateam06/minidarkmoodle/module/cours/Cours.java
+++ src/main/java/com/lateam06/minidarkmoodle/module/cours/Cours.java
@@ -1,6 +1,13 @@
 package com.lateam06.minidarkmoodle.module.cours;
 
+import com.lateam06.minidarkmoodle.module.AbstractRessource;
 import com.lateam06.minidarkmoodle.module.Ressource;
 
-public class Cours implements Ressource {
+public class Cours extends AbstractRessource {
+
+    public Cours(String name, String desc, int id) {
+        super(name, desc, id);
+    }
+    
+    
 }
