dcab85ea70bf47753bab5cc2dfecd9115dd617b3
Antoine <monsieur.cousson@gmail.com>
2022-02-03 17:35:07 +0100

    questionnaire squeletton

--- src/main/java/com/lateam06/minidarkmoodle/module/questionnaire/Question.java
+++ src/main/java/com/lateam06/minidarkmoodle/module/questionnaire/Question.java
@@ -1,4 +1,27 @@
 package com.lateam06.minidarkmoodle.module.questionnaire;
 
 public class Question {
+    private final int id;
+    private final int num;
+    private final String desc;
+
+
+    public Question(int id, int num, String description) {
+        this.id = id;
+        this.num = num;
+        this.desc = description;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
 }
--- src/main/java/com/lateam06/minidarkmoodle/module/questionnaire/Questionnaire.java
+++ src/main/java/com/lateam06/minidarkmoodle/module/questionnaire/Questionnaire.java
@@ -1,6 +1,36 @@
 package com.lateam06.minidarkmoodle.module.questionnaire;
 
+import com.lateam06.minidarkmoodle.module.AbstractRessource;
 import com.lateam06.minidarkmoodle.module.Ressource;
 
-public class Questionnaire implements Ressource {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Questionnaire extends AbstractRessource {
+    private final String name;
+    private final String desc;
+    private final int id;
+
+  private final List<Question> questions;
+    public Questionnaire(String name, String desc, int id) {
+        super(name,desc,id);
+        this.name = name;
+        this.desc = desc;
+        this.id = id;
+
+        this.questions = new ArrayList<>();
+    }
+
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+    public void addQuestion(Question q){
+        questions.add(q);
+    }
+    public void removeQuestion(Question q ){
+        questions.remove(q);
+    }
+
+
 }
