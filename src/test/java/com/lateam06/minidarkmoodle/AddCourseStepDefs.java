ec7b56fe6d5c39958593042b8f73d4bdd4cdef91
Jean Philippe Carlens <coruscant11@gmail.com>
2022-02-03 17:56:51 +0100

    Modifcation of features AddCourse.feature et AddQuiz.feature The data are real #1

---
+++ src/test/java/com/lateam06/minidarkmoodle/AddCourseStepDefs.java
@@ -0,0 +1,8 @@
+package com.lateam06.minidarkmoodle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AddCourseStepDefs {
+
+
+}
---
+++ src/test/java/com/lateam06/minidarkmoodle/AddCourseStepDefsTest.java
@@ -0,0 +1,23 @@
+package com.lateam06.minidarkmoodle;
+
+import io.cucumber.java.en.Given;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddCourseStepDefsTest {
+
+    /*private Module m;
+    private Teacher marcel;
+    private Course c;*/
+
+    public AddCourseStepDefsTest() {
+
+    }
+
+    @Given("a teacher named {string} and with teacher ID {int}")
+    public void givenATeacher(String teacherName, int teacherId) {
+
+    }
+
+}
