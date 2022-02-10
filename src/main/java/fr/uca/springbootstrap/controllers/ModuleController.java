9b781d8dd210fe384ab27027ebdaae12e3000c51
Yann-Brault <brlt.yann@gmail.com>
2022-02-10 12:03:50 +0100

    delete mapping user #17

--- src/main/java/fr/uca/springbootstrap/controllers/ModuleController.java
+++ src/main/java/fr/uca/springbootstrap/controllers/ModuleController.java
@@ -208,6 +208,38 @@ public class ModuleController {
 
     }
 
+    @DeleteMapping("/{id}/participants/{userid}")
+    @PreAuthorize("hasRole('TEACHER')")
+    public ResponseEntity<?> removeUser(Principal principal, @PathVariable long id, @PathVariable long userid) {
+        Optional<Module> omodule = moduleRepository.findById(id);
+        Optional<User> ouser = userRepository.findById(userid);
+        if (!omodule.isPresent()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: No such module"));
+        }
+        if (!ouser.isPresent()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: not such user"));
+        }
+
+        Module module = omodule.get();
+        User user = ouser.get();
+        User actor = userRepository.findByUsername(principal.getName()).get();
+
+        Set<User> participants = module.getParticipants();
+        if ((participants.isEmpty() && actor.equals(user))
+                || participants.contains(actor)) {
+            participants.remove(user);
+        } else {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Not allowed to remove user!"));
+        }
+        moduleRepository.save(module);
+        return ResponseEntity.ok((new MessageResponse("User successfully remove from module")));
+    }
 
     User createUser(String userName, String email, String password, Set<String> strRoles) {
         User user = new User(userName, email, password);
