f9eacb711aa3b61b8b58556597f3345511d74689
MonsieurCo <monsieur.cousson@gmail.com>
2022-02-17 09:18:42 +0100

    api code runner

--- coderunner/src/main/java/code/runner/CodeRunnerApi.java
+++ coderunner/src/main/java/code/runner/CodeRunnerApi.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class,org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class CodeRunnerApi {
 
 	public static void main(String[] args) {
